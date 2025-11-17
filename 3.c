429e26a8b28ada054d5d7108a89114f6847bdcbf
Nandan-ai-creator <snandan233@gmail.com>
2025-11-17 20:44:27 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    printf("The main() function in c is the starting point of execution of the pgm\n");
+    printf("without main() compiler will not execute the program\n");
+    printf("it tells the compiler from where the  program execution starts\n");
+    return 0;
+    
+}
